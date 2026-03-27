f12c78f075b9959b718f5249dff914fc5b133ce8
holiday-sean <sean.holidayCA@gmail.com>
2026-03-26 23:30:48 -0700

    Added tests for and abstracted 02 script

--- src/02_clean_airbnb_data.R
+++ src/02_clean_airbnb_data.R
@@ -16,76 +16,13 @@ library(stringr)
 library(tidyr)
 library(docopt)
 
+source("R/clean_airbnb_data.R")
+
 doc <- "Usage:
     02_clean_airbnb_data.R --input=<input> --out=<out>
 "
 
 opt <- docopt(doc)
-
-# Define a data cleaning function
-clean_airbnb_data <- function(raw_airbnb) {
-
-    # Set a random seed to ensure reproducibility
-    set.seed(310)
-
-    # Keep the target variable and most relevant columns
-    # Keep 'id' as a character variable
-    # Remove rows where 'price' is empty, and transfer 'price' into a numeric variable
-    airbnb <- raw_airbnb %>%
-      select(
-        id, host_is_superhost, neighbourhood_cleansed, property_type, room_type, 
-        accommodates, bathrooms, bedrooms, price, review_scores_rating, reviews_per_month
-      ) %>%
-      mutate(id = as.character(id)) %>%
-      filter(!is.na(price), price != "") %>%
-      mutate(
-        price = price %>%
-          str_remove_all("[$,]") %>%
-          as.numeric()
-      ) %>%
-      filter(!is.na(price)) %>%
-      # Explicitly drop rows with missing values in your model's predictors
-      drop_na(accommodates, bedrooms, bathrooms, room_type, property_type)
-
-    # Refactor 'host_is_superhost' to 'Yes', 'No', "Unknown'
-    airbnb <- airbnb %>%
-      mutate(
-        host_is_superhost = case_when(
-          host_is_superhost == TRUE ~ "Yes",
-          host_is_superhost == FALSE ~ "No",
-          is.na(host_is_superhost) ~ "Unknown",
-          TRUE ~ "Unknown"
-        ),
-        host_is_superhost = as.factor(host_is_superhost)
-      )
-
-    # Find out the 4 most frequent property types
-    top4_property <- airbnb %>%
-      count(property_type, sort = TRUE) %>%
-      slice_head(n = 4) %>%
-      pull(property_type)
-    
-    # Merge the remaining values into "Other"
-    airbnb <- airbnb %>%
-      mutate(property_type = ifelse(property_type %in% top4_property,
-                                    property_type,
-                                    "Other"),
-             property_type = as.factor(property_type))
-
-    # Find out the 4 most frequent neighbourhoods
-    top4_neighbourhoods <- airbnb %>%
-      count(neighbourhood_cleansed, sort = TRUE) %>%
-      slice_head(n = 4) %>%
-      pull(neighbourhood_cleansed)
-    
-    # Merge the remaining values into "Other" and rename the column
-    airbnb <- airbnb %>%
-      mutate(neighbourhood = ifelse(neighbourhood_cleansed %in% top4_neighbourhoods,
-                                    neighbourhood_cleansed,
-                                    "Other"),
-             neighbourhood = as.factor(neighbourhood))
-}
-
 
 main <- function() {
     input <- opt$input
---
+++ tests/testthat/test-clean_airbnb_data.R
@@ -0,0 +1,94 @@
+library(testthat)
+library(dplyr)
+library(readr)
+library(stringr)
+library(tidyr)
+library(docopt)
+source("../../R/clean_airbnb_data.R")
+
+test_that("`clean_airbnb_data` should return a data frame", {
+  clean_df <- clean_airbnb_data(one_raw_instance_obs)
+  expect_s3_class(clean_df, "data.frame")
+})
+
+test_that("`clean_airbnb_data` should return a data frame with 12 columns", {
+  clean_df <- clean_airbnb_data(one_raw_instance_obs)
+  expect_equal(ncol(clean_df), 12)
+})
+
+test_that("`clean_airbnb_data` should return correct column types", {
+  clean_df <- clean_airbnb_data(one_raw_instance_obs)
+  expect_type(clean_df$id, "character")
+  expect_s3_class(clean_df$host_is_superhost, "factor")
+  expect_s3_class(clean_df$property_type, "factor")
+  expect_s3_class(clean_df$neighbourhood, "factor")
+  expect_type(clean_df$price, "double")
+})
+
+test_that("`clean_airbnb_data` should return the correct cleaned output", {
+  clean_df <- clean_airbnb_data(one_raw_instance_obs)
+  expect_equal(clean_df$id, "1")
+  expect_equal(as.character(clean_df$host_is_superhost), "Yes")
+  expect_equal(as.character(clean_df$property_type), "House")
+  expect_equal(clean_df$price, 150)
+  expect_equal(as.character(clean_df$neighbourhood), "Downtown")
+})
+
+test_that("`clean_airbnb_data` should return 1 row when input has no NA values", {
+  clean_df <- clean_airbnb_data(zero_na_obs)
+  expect_equal(nrow(clean_df), 1)
+})
+
+test_that("`clean_airbnb_data` should return 0 rows when price is NA", {
+  clean_df <- clean_airbnb_data(price_na_obs)
+  expect_equal(nrow(clean_df), 0)
+})
+
+test_that("`clean_airbnb_data` should remove all rows with NA values and return only complete cases", {
+  clean_df <- clean_airbnb_data(all_na_except_one_obs)
+  expect_equal(nrow(clean_df), 1)
+  expect_false(any(is.na(clean_df)))
+})
+
+test_that("`clean_airbnb_data` should correctly refactor `host_is_superhost` to Yes, No, or Unknown", {
+
+   clean_df <- clean_airbnb_data(superhost_obs)
+
+   expect_equivalent(clean_df, superhost_exp)
+   expect_equal(clean_df$host_is_superhost, factor(c("Yes", "No", "Unknown"), 
+                                                  levels = c("No", "Unknown", "Yes")))
+})
+
+test_that("`clean_airbnb_data` should only retain the top four most frequent property types and merge the rest into Other", {
+   
+   clean_df <- clean_airbnb_data(top_four_prop_type_obs)
+   
+   # expect_setequal checks if the items match, regardless of order
+   expect_setequal(levels(clean_df$property_type), expected_prop_type_levels)
+   
+   # Check that there are 5 unique levels (Top 4 + Other)
+   expect_equal(length(levels(clean_df$property_type)), 5)
+   
+   # We check indices 3 and 8 specifically:
+   expect_true(all(clean_df$property_type[c(3, 8)] == "Other"))
+   
+   # We can also verify that the others did NOT become "Other"
+   expect_false(any(clean_df$property_type[c(1, 2, 4, 5, 6, 7, 9, 10)] == "Other"))
+})
+
+test_that("`clean_airbnb_data` should only retain the top four most frequent neighbourhoods and merge the rest into Other", {
+  clean_df <- clean_airbnb_data(top_four_neighbour_obs)
+   
+   # expect_setequal checks if the items match, regardless of order
+   expect_setequal(levels(clean_df$neighbourhood), expected_neighbourhood_levels)
+   
+   # Check that there are 5 unique levels (Top 4 + Other)
+   expect_equal(length(levels(clean_df$neighbourhood)), 5)
+   
+   # We check indices 3 and 8 specifically:
+   expect_true(all(clean_df$neighbourhood[c(1, 10)] == "Other"))
+   
+   # We can also verify that the others did NOT become "Other"
+   expect_false(any(clean_df$neighbourhood[c(2, 3, 4, 5, 6, 7, 8, 9)] == "Other"))
+})
+
